b84d210b49b6d36dc17c75425b3aaff1e092752e
jenniferteresageorge <69136165+jenniferteresageorge@users.noreply.github.com>
2025-04-24 17:47:36 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 from models import EmailClassifier
 import argparse
-import pandas as pd
+
 def main():
     parser = argparse.ArgumentParser(description="Train email classification model")
     parser.add_argument("--csv_path", type=str, required=True, help="Path to CSV dataset")
@@ -16,25 +16,6 @@ def main():
         text_col=args.text_col,
         label_col=args.label_col
     )
-
-from nlpaug import Augmenter
-from nlpaug.util import Action
-
-def augment_data(df, text_col, label_col, n_aug=3):
-    aug = Augmenter(
-        action=Action.SUBSTITUTE,
-        name='word2vec',
-        aug_min=1,
-        aug_max=3
-    )
-    
-    augmented = []
-    for _, row in df.iterrows():
-        for _ in range(n_aug):
-            new_text = aug.augment(row[text_col])
-            augmented.append({text_col: new_text, label_col: row[label_col]})
-    
-    return pd.concat([df, pd.DataFrame(augmented)], ignore_index=True)
 
 if __name__ == "__main__":
     main()
